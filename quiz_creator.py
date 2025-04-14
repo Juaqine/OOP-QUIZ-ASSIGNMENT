dc202602d14cf4022ec079d1e317aa02ffcba7a4
Juaqine <juaqineynicoavila@gmail.com>
2025-04-15 01:47:42 +0800

    pseudocode

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,1 +1,13 @@
+#Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
+#Write the collected data to a text file. Ask another question until the user chose to exit.
 
+#set filename to save the quiz data
+#display welcome message
+#begin an infinite loop that asks the user for questions and choices
+#ask the user for correct answer
+
+#open the file in append mode to save the questions and choices
+#ask the user if they want to add a question
+#if yes, continue asking questions
+#if no, end the program
+#display goodbye message

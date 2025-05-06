f097dc3e0fcbb60674918cb1f1ad64225b41a8ea
Juaqine <juaqineynicoavila@gmail.com>
2025-05-06 13:24:06 +0800

    fixed line

--- Quiz App/quiz_app.py
+++ Quiz App/quiz_app.py
@@ -97,6 +97,3 @@ class QuizApp:
         self.question_label.pack(pady=30)
 
         self.buttons = {}
-
-        self.question_label = tk.Label(root, font=("Arial", 18), bg=MAROON, fg=YELLOW, wraplength=550)
-        self.question_label.pack(pady=30)
